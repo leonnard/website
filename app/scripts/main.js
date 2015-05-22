2753eb5b21fc3ac66db67c9539660648076be64c
moklick <info@moritzklack.com>
2015-05-22 16:56:25 +0200

    update styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 var $ = require('jquery');
 var utils = require('./utils');
-var nav = require('./nav');
+//var nav = require('./nav');
 var contact = require('./contact');
 var map = require('./map')
 
@@ -11,13 +11,13 @@ map.init();
 //glitcher.init(document.getElementById('canvas'));
 
 (function initEvents(window, document, $) {
- /* $('.nav-toggle').on(utils.click, nav.toggle);
+  /*$('.nav-toggle').on(utils.click, nav.toggle);
   $('.nav-close').on(utils.click, nav.close);
   $('nav > .navigation li, .mobile-nav li').on(utils.click, nav.onClick);
   $('nav > .logo').on(utils.click, nav.scrollToStart);
-  $(window).on('scroll', nav.onScroll);
+  $(window).on('scroll', nav.onScroll);*/
 
   $('#contact-form').on('submit', contact.submit);
   $('#contact-form textarea').on('focus', contact.activate);
-  $('#contact-form textarea').on('blur', contact.leave);*/
+  $('#contact-form textarea').on('blur', contact.leave);
 })(window, document, $);
--- app/scripts/map.js
+++ app/scripts/map.js
@@ -10,31 +10,31 @@ var geojson = {
         "coordinates": [
           [
             [
-              13.422361314296722,
+              13.422363996505737,
               52.50084916515129
             ],
             [
-              13.422323763370514,
-              52.50079854851265
-            ],
-            [
-              13.42240422964096,
-              52.500777322162946
-            ],
-            [
-              13.422508835792542,
-              52.50092427360445
-            ],
-            [
-              13.422347903251648,
-              52.50097325730911
-            ],
-            [
-              13.422270119190214,
+              13.42227280139923,
               52.50087528984518
             ],
             [
-              13.422361314296722,
+              13.422347903251648,
+              52.50097162451984
+            ],
+            [
+              13.422511518001556,
+              52.50092427360445
+            ],
+            [
+              13.4223935008049,
+              52.50076752538272
+            ],
+            [
+              13.42231571674347,
+              52.500788751737126
+            ],
+            [
+              13.422363996505737,
               52.50084916515129
             ]
           ]
@@ -45,25 +45,29 @@ var geojson = {
 };
 
 var myStyle = {
-    fillColor: '#FFE50A',
+    fillColor: '#fff', //'#FFE50A',
     fillOpacity: 0.25,
-    color: '#bda800',
+    color:'#fff', //'#bda800',
     weight: 1,
     opacity: 0.85,
-    stroke: true
+    stroke: true,
+    radius: 7
 };
 
+var center = [52.5008, 13.422294];
 
 function init(){
    var map = new L.Map('map', {
-      center: [52.5008, 13.422294],
+      center: center,
       zoom: 18,
       minZoom: 13,
-      //zoomControl: false,
+      zoomControl: false,
       touchZoom: false,
       scrollWheelZoom: false,
       doubleClickZoom: false,
-      boxZoom: false
+      boxZoom: false,
+      dragging : false,
+      tap: false
     });
       
     // http://{s}.tiles.mapbox.com/v3/moklick.icjhce78/{z}/{x}/{y}.png
@@ -72,6 +76,11 @@ function init(){
       //attribution: '<a href="http://lokaler.de">Lokaler</a> - <a href="http://www.stadtentwicklung.berlin.de/geoinformation/geodateninfrastruktur/de/geodienste/wms_titel.shtml" target="_blank">Geoportal Berlin</a> | Kartenserver: <a href="http://https://geodienste.lyrk.de/" target="_blank">Lyrk</a>'
       attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
     }).addTo(map);
+
+    var myIcon = L.divIcon({className: 'map-label', html : 'You can find us here.', iconSize: [150]});
+
+    L.marker([52.50095, 13.42257], {icon: myIcon }).addTo(map);
+    //L.circleMarker([52.50087, 13.4224], myStyle).addTo(map)
 
     L.geoJson(geojson, {
       style: myStyle
